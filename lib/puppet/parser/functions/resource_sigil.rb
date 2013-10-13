78777c426f01fac9ec99dbf83dc25bd6afb056d2
Tray Torrance <devwork@warrentorrance.com>
2013-10-12 19:58:58 -0700

    Attempt to tame function

--- lib/puppet/parser/functions/resource_sigil.rb
+++ lib/puppet/parser/functions/resource_sigil.rb
@@ -1,9 +1,13 @@
 # Many thanks to finch for basically writing this function
 module Puppet::Parser::Functions
   newfunction(:resource_sigil, :type => :rvalue) do |args|
-    return '@'  if resource.virtual?
-    return '@@' if resource.exported?
-    return ''
+    if resource.virtual?
+      '@'
+    elsif resource.exported?
+      '@@'
+    else
+      ''
+    end
   end
 end
 
