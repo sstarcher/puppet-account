1ec86643ee6d6068b7f7b37bf7d7358b11f2ad3a
Tray Torrance <devwork@warrentorrance.com>
2013-10-12 19:54:35 -0700

    Remove nested ternary

--- lib/puppet/parser/functions/resource_sigil.rb
+++ lib/puppet/parser/functions/resource_sigil.rb
@@ -1,7 +1,9 @@
 # Many thanks to finch for basically writing this function
 module Puppet::Parser::Functions
   newfunction(:resource_sigil, :type => :rvalue) do |args|
-    return (scope.resource.virtual? ? '@' : (scope.resource.exported? ? '@@' : ''))
+    return '@'  if resource.virtual?
+    return '@@' if resource.exported?
+    return ''
   end
 end
 
