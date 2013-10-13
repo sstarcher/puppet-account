f8c18336c2fda3dd6f31a05ecc946d125626db48
Tray Torrance <devwork@warrentorrance.com>
2013-10-12 19:34:16 -0700

    Accept arguments in resource_sigil

--- lib/puppet/parser/functions/resource_sigil.rb
+++ lib/puppet/parser/functions/resource_sigil.rb
@@ -1,6 +1,6 @@
 # Many thanks to finch for basically writing this function
 module Puppet::Parser::Functions
-  newfunction(:resource_sigil, :type => :rvalue) do
+  newfunction(:resource_sigil, :type => :rvalue) do |args|
     return (scope.resource.virtual? ? '@' : (scope.resource.exported? ? '@@' : ''))
   end
 end
