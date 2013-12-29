88b04f9e178e31877b65f85970b3de6d9707c9c0
Shane Starcher <shanestarcher@gmail.com>
2013-12-29 11:02:05 -0500

    Adding params.pp to support common

---
+++ manifests/params.pp
@@ -0,0 +1,24 @@
+# === Class: nexus::params
+#
+# module parameters.
+#
+# === Parameters
+#
+# NONE
+#
+# === Examples
+#
+# class nexus inherits nexus::params {
+# }
+#
+# === Authors
+#
+# Tom McLaughlin <tmclaughlin@hubspot.com>
+#
+# === Copyright
+#
+# Copyright 2013 Hubspot
+#
+class account::params($ssh_key = undef, $password = '!') {
+	
+}
